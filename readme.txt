e8354aed9f2f06cb81436a0ad5ef4ae786bc6be3
Ahmad Awais <mrahmadawais@gmail.com>
2017-11-14 15:15:58 +0500

    Version 2.0.0 💯

--- readme.txt
+++ readme.txt
@@ -1,17 +1,16 @@
 === Remove WP Dashboard Extra Widgets ===
 
-Contributors: mrahmadawais
-Donate link: http://AhmadAwais.com/contact
+Contributors: mrahmadawais, maedahbatool, wptie
+Donate link: https://AhmadAwais.com/contact
 Tags: remove, admin, dashboard, widgets
 Requires at least: 3.0
-Tested up to: 4.0
-Stable tag: 1.1
+Tested up to: 5.0
+Stable tag: 2.0
 License: GPLv2 or later
 
 
 == Description ==
 Removes the WordPress dashboard widgets that are extra and useless for some users i.e. plugins, wp blog news etc
-
 
 == Installation ==
 
@@ -22,7 +21,7 @@ Upload the  plugin to your blog and just activate it.
 = 1.1 =
 Several minor updates
 
-= 1.0 = 
+= 1.0 =
 It is the first release !
 
 == Upgrade Notice ==
@@ -32,7 +31,7 @@ No upgrade till yet
 Visit http://freakify.com for screenshots and details
 http://freakify.com/2011/08/wp-remove-dashboard-extra-widgets-plugin-of-wordpress-by-ahmad-awais/
 
-== Frequently Asked Questions == 
+== Frequently Asked Questions ==
 How can i install it?
 Ans: Upload the  plugin to your blog and just activate it.
 
--- remove-wp-dashboard-extra-widgets.php
+++ remove-wp-dashboard-extra-widgets.php
@@ -1,25 +1,37 @@
 <?php
-/*
-Plugin Name: Remove WP Dashboard Extra Widgets by Ahmad Awais
-Plugin URI: http://freakify.com/2011/08/wp-remove-dashboard-extra-widgets-plugin-of-wordpress-by-ahmad-awais/
-Description: Removes the WordPress dashboard widgets that are extra and useless i.e. plugins, wp blog news etc
-Version: 1.1
-Author: Ahmad Awais
-Author URI: http://AhmadAwais.com/
-License:GPL2
+/**
+ * Plugin Name: WP Remove Dashboard Extra Widgets
+ * Plugin URI: https://AhmadAwais.com/
+ * Description: Removes the WordPress dashboard widgets that are extra and useless i.e. plugins, wp blog news etc.
+ * Author: WPCouple(Ahmad Awais & Maedah Batool)
+ * Author URI: https://AhmadAwais.com/
+ * Version: 2.0.0
+ * License: GPL2+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package WP
+ */
 
-*/
-
-
-
-
-add_action( 'wp_dashboard_setup', 'remove_wp_dashboard_extra_widgets' );
-
-function remove_wp_dashboard_extra_widgets() {
-	remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
-	remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
-	remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
+// Hook.
+add_action('wp_dashboard_setup', 'wpc_remove_dashboard_widgets', 9999 );
 
+/**
+ * Removes all dashboard widgets.
+ *
+ * @since 1.0.0
+ */
+function wpc_remove_dashboard_widgets() {
+	// Get global obj.
+	global $wp_meta_boxes;
 
+	// Left side metaboxes.
+	$wp_meta_boxes['dashboard']['normal']['core'] = array();
+
+	// Right side metaboxes.
+	$wp_meta_boxes['dashboard']['side']['core'] = array();
+}
